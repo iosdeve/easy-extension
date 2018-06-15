b713a3ba0a200916cecc1c8b56329f0631219495
Xin Chen <steven.amaxgs@gmail.com>
2018-06-15 16:16:05 +0800

    Update 001_create_dbtable.sql
    update db scripts

--- easy-extension-core/src/main/resources/dbscripts/001_create_dbtable.sql
+++ easy-extension-core/src/main/resources/dbscripts/001_create_dbtable.sql
@@ -1,10 +1,36 @@
-Create Table If Not Exists `app` (
+CREATE TABLE `app` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `appName` varchar(100) DEFAULT NULL,
+  `appDesc` varchar(800) DEFAULT NULL,
   `actionClass` varchar(100) DEFAULT NULL,
   `appPath` varchar(100) DEFAULT NULL,
   `userid` int(11) DEFAULT NULL,
   `created` datetime DEFAULT NULL,
   `lastUpdated` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
+
+CREATE TABLE `files` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `appId` int(11) DEFAULT NULL,
+  `fileName` varchar(100) DEFAULT NULL,
+  `name` varchar(100) DEFAULT NULL,
+  `filesize` int(11) DEFAULT NULL,
+  `filePath` varchar(150) DEFAULT NULL,
+  `userid` int(11) DEFAULT NULL,
+  `created` datetime DEFAULT NULL,
+  `lastUpdated` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=46 DEFAULT CHARSET=utf8;
+
+CREATE TABLE `nodetable` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `parentId` int(11) DEFAULT NULL,
+  `description` varchar(200) DEFAULT NULL,
+  `name` varchar(200) DEFAULT NULL,
+  `referId` int(11) DEFAULT NULL,
+  `created` datetime DEFAULT NULL,
+  `lastUpdated` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=35 DEFAULT CHARSET=utf8;
+
